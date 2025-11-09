68140fa737d6bbf826cad54a88ee765a4730e077
Arthur Teixeira <arthurteixeira.guts@gmail.com>
2025-11-09 20:10:37 -0300

    Integrate Swagger for API documentation and configure dependency injection for ProductService and IProductRepository. Add controller mapping and enhance weather forecast endpoint setup.

--- Program.cs
+++ Program.cs
@@ -1,25 +1,32 @@
-using ProductApi.Api.Endpoints;
 using ProductApi.Application.Interfaces;
 using ProductApi.Application.Services;
 using ProductApi.Domain.Interfaces;
 using ProductApi.Infrastructure.Providers;
+using ProductApi.Infrastructure.Repositories;
+using ProductApi.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ProductService>();
 
 builder.Services.AddSingleton<IWeatherForecastProvider, RandomWeatherForecastProvider>();
 builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+app.MapControllers();
 app.MapWeatherForecastEndpoints();
 
 app.Run();
