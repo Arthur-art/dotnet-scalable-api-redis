448fabb4d35f485d14ccf957f1650055811b5b57
Arthur Teixeira <arthurteixeira.guts@gmail.com>
2025-11-09 20:24:40 -0300

    Refactor Program.cs to streamline service registrations. Remove unused weather forecast provider and endpoint mappings while maintaining Redis caching and Swagger configuration.

--- Program.cs
+++ Program.cs
@@ -1,25 +1,20 @@
-using ProductApi.Application.Interfaces;
 using ProductApi.Application.Services;
-using ProductApi.Domain.Interfaces;
-using ProductApi.Infrastructure.Providers;
 using ProductApi.Infrastructure.Repositories;
-using ProductApi.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ProductService>();
 
-builder.Services.AddSingleton<IWeatherForecastProvider, RandomWeatherForecastProvider>();
-builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = "localhost:6379";
     options.InstanceName = "ProductApi_";
 });
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -32,6 +27,5 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapControllers();
-app.MapWeatherForecastEndpoints();
 
 app.Run();
